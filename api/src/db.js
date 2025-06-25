8ffee0f07b0f78580358a9dda5761e988a381c63
anitanersesyan <anita140701@gmail.com>
2025-06-25 22:55:00 +0200

    Added the files for react2 week1

--- api/src/db.js
+++ api/src/db.js
@@ -1,15 +1,20 @@
-import knex from "knex";
 import "dotenv/config";
+import knex from "knex";
 
-const db = knex({
-  client: process.env.DB_CLIENT,
+// First create the configuration object
+const dbConfig = {
+  client: process.env.DB_CLIENT || "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: parseInt(process.env.DB_PORT || "3306", 10),
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE_NAME || "meal_sharing",
   },
-});
+  debug: true,
+};
+
+// Then initialize the db connection
+const db = knex(dbConfig);
 
 export default db;
