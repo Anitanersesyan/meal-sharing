b37fc5067ed630cec2cff34f1e2f4fbab49d4922
anitanersesyan <anita140701@gmail.com>
2025-06-25 22:26:51 +0200

    Added files for react2 week1

--- api/src/db.js
+++ api/src/db.js
@@ -1,15 +1,17 @@
-import knex from "knex";
 import "dotenv/config";
+import knex from "knex";
 
-const db = knex({
+const dbConfig = {
   client: process.env.DB_CLIENT,
   connection: {
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    port: parseInt(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
   },
-});
+};
+
+const db = knex(dbConfig);
 
 export default db;
