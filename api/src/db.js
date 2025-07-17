3d4a547886190d2699a0908c7bcda1cfefb919ef
anitanersesyan <anita140701@gmail.com>
2025-07-17 12:34:08 +0200

    Add PostgreSQL connection config

--- api/src/db.js
+++ api/src/db.js
@@ -1,20 +1,19 @@
 import "dotenv/config";
 import knex from "knex";
 
-// First create the configuration object
 const dbConfig = {
-  client: process.env.DB_CLIENT || "mysql2",
+  client: process.env.DB_CLIENT || "pg",
   connection: {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT || "3306", 10),
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_DATABASE_NAME || "meal_sharing",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    ssl: { rejectUnauthorized: false }, // Important for Render!
   },
   debug: true,
 };
 
-// Then initialize the db connection
 const db = knex(dbConfig);
 
 export default db;
